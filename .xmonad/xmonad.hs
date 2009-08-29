a68b939a703afe62044cd2f4b177dbbf3828a0f8
Michael Budde <mbudde@gmail.com>
2009-08-29 15:52:44 +0200

    [xmonad] Rewrite config file

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,79 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Util.EZConfig
+import XMonad.Actions.CycleWS
+import XMonad.Prompt
+import XMonad.Prompt.AppendFile
+import XMonad.Prompt.Shell
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+import Graphics.X11.Types
 
 
-myManageHook = composeAll
-    [ className =? "Pidgin"           --> doFloat
-    , title     =? "VLC media player" --> doFloat
-    , className =? "Pidgin"           --> doShift "im"
-    , className =? "Rhythmbox"        --> doShift "music"
-    , className =? "Evolution"        --> doShift "im"
-    , className =? "Do"               --> doIgnore ]
+myModMask = mod4Mask
 
 
-myWorkspaces = [ "code", "web", "im", "music" ] ++ map show [5..9]
+myWorkspaces = ["alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota"]
 
-main =
-    xmonad gnomeConfig
-    { manageHook = manageHook gnomeConfig <+> myManageHook
-    , modMask    = mod4Mask
-    , workspaces = myWorkspaces }
+
+myManageHook = composeAll . concat $
+    [ [className =? c      --> doFloat  | c <- cnFloats]
+    , [title     =? t      --> doFloat  | t <- tFloats]
+    , [className =? c      --> doIgnore | c <- cnIgnores]
+
+    , [className =? cn      --> action | (cn, action) <- cnHooks]
+    , [appName   =? an      --> action | (an, action) <- anHooks]
+    ]
+    where
+        cnFloats    = [ "Pidgin", "Gimp", "Transmission"
+                      , "Gnome-system-monitor", "Gnome-calculator", "Gcalctool" ]
+        tFloats     = [ "VLC media player" ]
+        cnIgnores   = [ "Do" ]
+
+        cnHooks     = [ ("Evolution",           doF (W.shift "gamma"))
+                      , ("Pidgin",              doF (W.shift "gamma"))
+                      , ("OpenOffice.org 3.0",  doF (W.view "zeta") <+> doF (W.shift "zeta"))
+                      , ("Gimp",                doF (W.view "eta") <+> doF (W.shift "eta"))
+                      , ("Nautilus",            doF (W.view "epsilon") <+> doF (W.shift "epsilon") <+> doF (W.swapUp))
+                      ]
+
+        anHooks     = [ ("Navigator",           doF (W.view "beta") <+> doF (W.shift "beta"))
+                      , ("Banshee",             doF (W.view "delta") <+> doF (W.shift "delta"))
+                      ]
+
+
+myKeysP = (
+    [ ("M-C-o",     shiftNextScreen)
+    , ("M-x",       shellPrompt myXPConfig)
+    , ("M-S-n",     appendFilePrompt myXPConfig "/home/michael/NOTES")
+    ] )
+
+myKeys = (
+    [ ((m .|. myModMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e] [1, 0]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    ++
+    [ ((m .|. myModMask, k), windows $ f i)
+        | (i, k) <- zip (myWorkspaces) [xK_1 .. xK_9]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    )
+
+
+myXPConfig = defaultXPConfig
+        { position = Bottom
+        , font = "-*-liberation.mono-medium-r-normal-*-13-*-*-*-*-*-*-*"
+        }
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = False
+
+
+main = xmonad $ gnomeConfig
+        { modMask           = myModMask
+        , workspaces        = myWorkspaces
+        , focusFollowsMouse = myFocusFollowsMouse
+        , manageHook        = manageHook gnomeConfig <+> myManageHook }
+        `additionalKeysP` myKeysP
+        `additionalKeys` myKeys
