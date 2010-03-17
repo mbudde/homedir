bc1db6e2a60d3c87be12607178eed81382f69cf1
root <root@ubuntu.(none)>
2010-03-17 21:38:34 +0000

    [emacs] org-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,3 +6,18 @@
 (ergoemacs-mode 1)
 
 (load "emacsd-tile.el")
+
+(add-to-list 'load-path "org-mode/lisp")
+;;(add-to-list 'load-path "org-mode/contrib/lisp")
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(setq org-log-done 'time)
+(setq org-todo-keywords
+      '((sequence "TODO" "STARTED" "WAITING" "|" "DONE")))
+(setq org-todo-keyword-faces
+      '(("STARTED"   . (:foreground "orange" :weight bold))
+        ("WAITING"   . (:foreground "blue" :weight bold))))
+
+(defun gtd ()
+  (interactive)
+  (find-file "/home/michael/gtd/gtd.org"))
