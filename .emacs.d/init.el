6008b5b0bc261ea3f3abe6f40bd7931c392cd509
Michael Budde <mbudde@gmail.com>
2010-03-25 17:17:08 +0100

    [emacs] Blueish theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,3 +6,11 @@
 (ergoemacs-mode 1)
 
 (load "emacsd-tile.el")
+
+(require 'color-theme)
+(require 'blueish-theme)
+(setq color-theme-is-global t)
+;;(color-theme-initialize)
+;;(color-theme-marquardt)
+(blueish-theme)
+
