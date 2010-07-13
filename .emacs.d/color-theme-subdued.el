53fe93535f91d4aded4bbae391a647c2a024e3b1
Michael Budde <mbudde@gmail.com>
2010-07-13 11:10:15 +0200

    [emacs] Update subdued color theme
    Inspiration from tangotango color theme.

--- .emacs.d/color-theme-subdued.el
+++ .emacs.d/color-theme-subdued.el
@@ -29,24 +29,24 @@ Based on the Tango color palette."
      ((foreground-color . "#d3d7cf")
       (background-color . "#000")
       (background-mode . dark)
-      (cursor-color . "#729fcf") ; medium chameleon
+      (cursor-color . "#729fcf")
       (mouse-color . "#73d216"))
 
      ;;; Standard font lock faces
      (default ((t (nil))))
-     (font-lock-comment-face ((t (:foreground "#61635e")))) ; dark aluminum
-     (font-lock-comment-delimiter-face ((t (:foreground "#61635e")))) ; dark aluminum
-     (font-lock-doc-face ((t (:foreground "#77507b")))) ; plum
-     (font-lock-doc-string-face ((t (:foreground "#77507b")))) ; plum
-     (font-lock-string-face ((t (:foreground "#77507b")))) ; plum
-     (font-lock-keyword-face ((t (:foreground "#729fcf")))) ; light sky blue
-     (font-lock-builtin-face ((t (:foreground "#855c1b")))) ; med-dark chocolate
-     (font-lock-function-name-face ((t (:foreground "#c4a000")))) ; dark butter
+     (font-lock-comment-face ((t (:foreground "#888a85"))))
+     (font-lock-comment-delimiter-face ((t (:foreground "#888a85"))))
+     (font-lock-doc-face ((t (:foreground "#888a85"))))
+     (font-lock-doc-string-face ((t (:foreground "#888a85"))))
+     (font-lock-string-face ((t (:foreground "#ad7fa8"))))
+     (font-lock-keyword-face ((t (:foreground "#729fcf"))))
+     (font-lock-builtin-face ((t (:foreground "#855c1b"))))
+     (font-lock-function-name-face ((t (:foreground "#edd400"))))
      (font-lock-variable-name-face ((t (nil))))
-     (font-lock-preprocessor-face ((t (:foreground "#888a85")))) ; aluminum
-     (font-lock-constant-face ((t (:foreground "#4e9a06")))) ; dark chameleon
-     (font-lock-type-face ((t (:foreground "#ad7fa8")))) ; light plum
-     (font-lock-warning-face ((t (:bold t :foreground "#cc0000")))) ; scarlet red
+     (font-lock-preprocessor-face ((t (:foreground "#888a85"))))
+     (font-lock-constant-face ((t (:foreground "#8ae234"))))
+     (font-lock-type-face ((t (:foreground "#ad7fa8"))))
+     (font-lock-warning-face ((t (:bold t :foreground "#cc0000"))))
 
      ;; Search
      (isearch ((t (:foreground "#080808" :background "#edd400"))))
@@ -58,8 +58,8 @@ Based on the Tango color palette."
      (mode-line ((t (:background "#1f1f1f" :foreground "#eeeeec"))))
      (mode-line-buffer-id ((t (:background "#1f1f1f" :foreground "#eeeeec"))))
      (mode-line-inactive ((t (:background "#1f1f1f" :foreground "#888a85"))))
-     (minibuffer-prompt ((t (:foreground "#729fcf")))) ; light sky blue
-     (region ((t (:background "#2e3436"))))
+     (minibuffer-prompt ((t (:foreground "#729fcf"))))
+     (region ((t (:background "dark slate blue"))))
 
      ;; Parenthesis matching
      ;; (show-paren-match-face ((t (:foreground "#2e3436" :background "#73d216"))))
@@ -82,31 +82,31 @@ Based on the Tango color palette."
      (diff-changed ((t (:foreground "#75507b")))
 
      ;; Calendar
-     (holiday-face ((t (:foreground "#cc0000")))) ; dark scarlet red
+     (holiday-face ((t (:foreground "#cc0000"))))
 
      ;; Info
-     (info-xref ((t (:foreground "#729fcf")))) ; light sky blue
-     (info-xref-visited ((t (:foreground "#ad7fa8")))) ; light plum
+     (info-xref ((t (:foreground "#729fcf"))))
+     (info-xref-visited ((t (:foreground "#ad7fa8"))))
 
      ;;; AUCTeX
-     (font-latex-sectioning-5-face ((t (:foreground "#c4a000" :bold t)))) ; dark butter
-     (font-latex-bold-face ((t (:foreground "#4e9a06" :bold t)))) ; dark chameleon
-     (font-latex-italic-face ((t (:foreground "#4e9a06" :italic t)))) ; dark chameleon
-     (font-latex-math-face ((t (:foreground "#855c1b")))) ; med-dark chocolate
-     (font-latex-string-face ((t (:foreground "#77507b")))) ; plum
-     (font-latex-warning-face ((t (:foreground "#cc0000")))) ; dark scarlet red
-     (font-latex-slide-title-face ((t (:foreground "#c4a000")))) ; dark butter
+     (font-latex-sectioning-5-face ((t (:foreground "#c4a000" :bold t))))
+     (font-latex-bold-face ((t (:foreground "#4e9a06" :bold t))))
+     (font-latex-italic-face ((t (:foreground "#4e9a06" :italic t))))
+     (font-latex-math-face ((t (:foreground "#855c1b"))))
+     (font-latex-string-face ((t (:foreground "#77507b"))))
+     (font-latex-warning-face ((t (:foreground "#cc0000"))))
+     (font-latex-slide-title-face ((t (:foreground "#c4a000"))))
 
      ;;; post-mode
-     (post-emoticon-face ((t (:background "#edd400" :foreground "#000000")))) ; medium butter
-     (post-header-value-face ((t (:foreground "#4e9a06")))) ; dark chameleon
-     (post-header-keyword-face ((t (:foreground "#4e9a06" :bold t)))) ; dark chameleon
-     (post-signature-text-face ((t (:foreground "#cc0000")))) ; dark scarlet red
-     (post-quoted-text-face ((t (:foreground "#855c1b" :slant normal)))) ; med-dark chocolate
-     (post-double-quoted-text-face ((t (:foreground "#77507b" :slant normal)))) ; plum
-     (post-multiply-quoted-text-face ((t (:foreground "#61635e" :slant normal)))) ; dark aluminum
-     (post-email-address-text-face ((t (:foreground "#729fcf" :bold t)))) ; light sky blue
-     (post-url-face ((t (:foreground "#729fcf" :bold t)))) ; light sky blue
+     (post-emoticon-face ((t (:background "#edd400" :foreground "#000000"))))
+     (post-header-value-face ((t (:foreground "#4e9a06"))))
+     (post-header-keyword-face ((t (:foreground "#4e9a06" :bold t))))
+     (post-signature-text-face ((t (:foreground "#cc0000"))))
+     (post-quoted-text-face ((t (:foreground "#855c1b" :slant normal))))
+     (post-double-quoted-text-face ((t (:foreground "#77507b" :slant normal))))
+     (post-multiply-quoted-text-face ((t (:foreground "#61635e" :slant normal))))
+     (post-email-address-text-face ((t (:foreground "#729fcf" :bold t))))
+     (post-url-face ((t (:foreground "#729fcf" :bold t))))
 
      ;; ido-mode
      (ido-first-match-face ((t (:foreground "#eeeeec" :bold t))))
