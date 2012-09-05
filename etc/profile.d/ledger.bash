0747d5f830a010ce0bb4b0f124959c9b376ee97b
Michael Budde <mbudde@gmail.com>
2012-09-05 09:01:19 +0200

    [bash] Change ledger path

--- etc/profile.d/ledger.bash
+++ etc/profile.d/ledger.bash
@@ -1,3 +1,3 @@
-if [ -f "$HOME/Documents/Regnskab/ledger.dat" ]; then
-    export LEDGER="$HOME/Documents/Regnskab/ledger.dat"
+if [ -f "$HOME/Documents/Regnskab/current/ledger.dat" ]; then
+    export LEDGER="$HOME/Documents/Regnskab/current/ledger.dat"
 fi
