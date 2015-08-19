110ad9be12ff050e0ac6e1616b4a4499fc86e8d0
Michael Budde <mbu@jobindex.dk>
2015-08-19 09:08:33 +0200

    [bash] Don't assume multirust is installed

--- etc/profiles-available/rust.bash
+++ etc/profiles-available/rust.bash
@@ -1,2 +1,7 @@
-DEFAULT=$(cat ~/.multirust/default)
-add_path ~/.multirust/toolchains/$DEFAULT/share/man MANPATH
+if [[ -d ~/.multirust ]]; then
+    DEFAULT=$(cat ~/.multirust/default)
+    add_path ~/.multirust/toolchains/$DEFAULT/share/man MANPATH
+else
+    add_path ~/opt/rust/bin
+    add_path ~/opt/rust/share/man MANPATH
+fi
