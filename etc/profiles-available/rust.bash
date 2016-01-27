e7e9720eb450e0a7b35e3157c3357a9a9dd2c07e
Michael Budde <mbudde@gmail.com>
2016-01-27 18:40:57 +0100

    [bash] Add both stable and nightly binaries to PATH

--- etc/profiles-available/rust.bash
+++ etc/profiles-available/rust.bash
@@ -1,6 +1,8 @@
 if [[ -d ~/.multirust ]]; then
     DEFAULT=$(cat ~/.multirust/default)
     add_path ~/.multirust/toolchains/$DEFAULT/share/man MANPATH
+    add_path ~/.multirust/toolchains/stable/cargo/bin
+    add_path ~/.multirust/toolchains/nightly/cargo/bin
 else
     add_path ~/opt/rust/bin
     add_path ~/opt/rust/share/man MANPATH
