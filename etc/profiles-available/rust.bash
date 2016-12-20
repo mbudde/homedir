d570816e9bbac4cf2118380ffcfba4f457e3d3c3
Michael Budde <mbu@jobindex.dk>
2016-12-20 16:24:02 +0100

    [bash] Get Rust sysroot with rustc

--- etc/profiles-available/rust.bash
+++ etc/profiles-available/rust.bash
@@ -1,10 +1,2 @@
-if [[ -d ~/.multirust ]]; then
-    DEFAULT=$(cat ~/.multirust/default)
-    add_path ~/.multirust/toolchains/$DEFAULT/share/man MANPATH
-    add_path ~/.multirust/toolchains/stable/cargo/bin
-    add_path ~/.multirust/toolchains/nightly/cargo/bin
-else
-    add_path ~/opt/rust/bin
-    add_path ~/opt/rust/share/man MANPATH
-    add_path ~/opt/rust/lib LD_LIBRARY_PATH
-fi
+add_path ~/.cargo/bin
+export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
